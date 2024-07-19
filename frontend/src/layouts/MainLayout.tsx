6cc26dd63bbeb4bb61135c997efffc92bacc6dfa
Ben Lenser <lenser.ben@gmail.com>
2024-07-19 16:19:48 +0200

    Fix background gap

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -8,9 +8,7 @@ import { WebSocketProvider } from '../contexts/newsContext';
 export const MainLayout: React.FC = () => {
   return (
     <WebSocketProvider>
-      <Grid
-        container
-        spacing={1}
+      <div
         style={{
           width: '100%',
           minHeight: '100vh',
@@ -18,29 +16,43 @@ export const MainLayout: React.FC = () => {
           background: 'black',
         }}
       >
-        <Grid item xs={6}>
-          <Bestlist withAutoScroll={false} />
-        </Grid>
-        <Grid item xs={6}>
-          <Grid container spacing={1}>
-            <Grid item xs={6}>
-              <a href="/game">
-                <img
-                  className="animate__animated animate__pulse"
-                  src="images/logo.png"
-                  style={{ width: '100%', animationIterationCount: 'infinite' }}
-                />
-              </a>
-            </Grid>
-            <Grid item xs={6}>
-              <PictureSlideShow />
-            </Grid>
-            <Grid item xs={12}>
-              <Newsfeed animation="animate__animated animate__fadeInUp" />
+        <Grid
+          container
+          spacing={1}
+          style={{
+            width: '100%',
+            minHeight: '100vh',
+            // background: 'url("/images/background/background.jpg")',
+            background: 'black',
+          }}
+        >
+          <Grid item xs={6}>
+            <Bestlist withAutoScroll={false} />
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <a href="/game">
+                  <img
+                    className="animate__animated animate__pulse"
+                    src="images/logo.png"
+                    style={{
+                      width: '100%',
+                      animationIterationCount: 'infinite',
+                    }}
+                  />
+                </a>
+              </Grid>
+              <Grid item xs={6}>
+                <PictureSlideShow />
+              </Grid>
+              <Grid item xs={12}>
+                <Newsfeed animation="animate__animated animate__fadeInUp" />
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </div>
     </WebSocketProvider>
   );
 };
